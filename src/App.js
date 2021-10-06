035516b2494e98e223716fe88735785653611052
sauravrajleaf <saurved785@gmail.com>
2021-10-07 04:03:39 +0530

    Css completed & uploading is working

--- src/App.js
+++ src/App.js
@@ -2,14 +2,8 @@ import React from "react";
 
 import ImageUploader from "./components/ImageUploader";
 
-import "./App.css";
-
 const App = () => {
-  return (
-    <div className="App">
-      <ImageUploader />
-    </div>
-  );
+  return <ImageUploader />;
 };
 
 export default App;
--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,5 +1,8 @@
 import React, { useState } from "react";
 
+import "./ImageUploader.css";
+
+import image from "./image.jpg";
 const ImageUploader = () => {
   const [uploadImage, setUploadImage] = useState(null);
 
@@ -14,20 +17,43 @@ const ImageUploader = () => {
     reader.readAsDataURL(e.target.files[0]);
   };
 
+  const inputBtnActive = () => {
+    const input_active = document.querySelector("#input-active");
+    input_active.click();
+  };
+
   return (
-    <div className="container">
-      Add your image
-      <div className="image-area">
-        <h2>Upload your image here</h2>
-        <img src={uploadImage} alt="" id="img" className="img" />
+    <div className="main">
+      <div className="container">
+        <div className="wrapper">
+          <div className="image-area">
+            <img src={uploadImage} className="img" />
+          </div>
+          <div className="content">
+            <div className="icon">
+              <i className="fas fa-cloud-upload-alt"></i>
+            </div>
+            <div className="text">No file chosen.</div>
+          </div>
+          <div className="close-btn">
+            <i className="fas fa-times"></i>
+          </div>
+          <div className="file-name">File Name Here</div>
+        </div>
+
+        <input
+          type="file"
+          name="image-upload"
+          id="input-active"
+          accept="image/jpeg"
+          onChange={imageHandler}
+          hidden
+        />
+        <span className="span">Only jpeg or jpg files are allowed.</span>
+        <button className="select-btn" onClick={inputBtnActive}>
+          Select An Image
+        </button>
       </div>
-      <input
-        type="file"
-        name="image-upload"
-        id="input"
-        accept="image/jpeg"
-        onChange={imageHandler}
-      />
     </div>
   );
 };
